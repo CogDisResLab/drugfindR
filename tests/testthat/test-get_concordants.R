f27d48ff25a55416947ca0b3771d972a1170dd5e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-02-23 12:48:03 -0500

    Added the "any" variants for consensus concordants testing

--- tests/testthat/test-get_concordants.R
+++ tests/testthat/test-get_concordants.R
@@ -19,11 +19,11 @@ concordant_col_spec <- cols(
   pValue = col_double()
 )
 
-input_filenames <- expand_grid(signature = c("kd_signature", "oe_signature", "cp_signature"), direction = c("up", "down"), threshold = c(0.85)) %>% unite("filename") %>% pull(filename) %>% str_c(., "csv", sep = ".") %>% file.path("reference", .)
+input_filenames <- expand_grid(signature = c("kd_signature", "oe_signature", "cp_signature"), direction = c("any", "up", "down"), threshold = c(0.85)) %>% unite("filename") %>% pull(filename) %>% str_c(., "csv", sep = ".") %>% file.path("reference", .)
 
 input_data <- map(input_filenames, ~ read_csv(.x, col_types = signature_col_spec))
 
-reference_filenames <- expand_grid(concordant = "concordant", signature = c("kd_signature", "oe_signature", "cp_signature"), direction = c("up", "down"), threshold = c(0.85), output_lib = c("OE", "KD", "CP")) %>% unite("filename") %>% pull(filename) %>% str_c(., "csv", sep = ".") %>% file.path("reference", .)
+reference_filenames <- expand_grid(concordant = "concordant", signature = c("kd_signature", "oe_signature", "cp_signature"), direction = c("any", "up", "down"), threshold = c(0.85), output_lib = c("OE", "KD", "CP")) %>% unite("filename") %>% pull(filename) %>% str_c(., "csv", sep = ".") %>% file.path("reference", .)
 
 reference_data <- map(reference_filenames, ~ read_csv(.x, col_types = concordant_col_spec, na = c("")))
 
@@ -38,8 +38,8 @@ test_that("Error thrown at invalid library", {
 })
 
 # Testing Knockdown Signatures
-
-## Testing the results for the up filtered signature
+#
+## Testing the results for the any filtered signature
 test_that("Results from the up knockdown signature are correct with respect to OE",{
   r <- get_concordants(input_data[[1]], "OE")
   expect_equal(nrow(r), nrow(reference_data[[1]]))
@@ -58,66 +58,64 @@ test_that("Results from the up knockdown signature are correct with respect to C
   expect_equal(r, reference_data[[3]], ignore_attr = TRUE)
 })
 
-## Testing the results for the down filtered signature
-test_that("Results from the down knockdown signature are correct with respect to OE",{
+## Testing the results for the up filtered signature
+test_that("Results from the up knockdown signature are correct with respect to OE",{
   r <- get_concordants(input_data[[2]], "OE")
   expect_equal(nrow(r), nrow(reference_data[[4]]))
   expect_equal(r, reference_data[[4]], ignore_attr = TRUE)
 })
 
-test_that("Results from the down knockdown signature are correct with respect to KD",{
+test_that("Results from the up knockdown signature are correct with respect to KD",{
   r <- get_concordants(input_data[[2]], "KD")
   expect_equal(nrow(r), nrow(reference_data[[5]]))
   expect_equal(r, reference_data[[5]], ignore_attr = TRUE)
 })
 
-test_that("Results from the down knockdown signature are correct with respect to CP",{
+test_that("Results from the up knockdown signature are correct with respect to CP",{
   r <- get_concordants(input_data[[2]], "CP")
   expect_equal(nrow(r), nrow(reference_data[[6]]))
   expect_equal(r, reference_data[[6]], ignore_attr = TRUE)
 })
 
-# Testing Overexpression Signatures
-
-## Testing the results for the up filtered signature
-test_that("Results from the up knockdown signature are correct with respect to OE",{
+## Testing the results for the down filtered signature
+test_that("Results from the down knockdown signature are correct with respect to OE",{
   r <- get_concordants(input_data[[3]], "OE")
   expect_equal(nrow(r), nrow(reference_data[[7]]))
   expect_equal(r, reference_data[[7]], ignore_attr = TRUE)
 })
 
-test_that("Results from the up knockdown signature are correct with respect to KD",{
+test_that("Results from the down knockdown signature are correct with respect to KD",{
   r <- get_concordants(input_data[[3]], "KD")
   expect_equal(nrow(r), nrow(reference_data[[8]]))
   expect_equal(r, reference_data[[8]], ignore_attr = TRUE)
 })
 
-test_that("Results from the up knockdown signature are correct with respect to CP",{
+test_that("Results from the down knockdown signature are correct with respect to CP",{
   r <- get_concordants(input_data[[3]], "CP")
   expect_equal(nrow(r), nrow(reference_data[[9]]))
   expect_equal(r, reference_data[[9]], ignore_attr = TRUE)
 })
 
-## Testing the results for the down filtered signature
-test_that("Results from the down knockdown signature are correct with respect to OE",{
+# Testing Overexpression Signatures
+
+## Testing the results for the any filtered signature
+test_that("Results from the up knockdown signature are correct with respect to OE",{
   r <- get_concordants(input_data[[4]], "OE")
   expect_equal(nrow(r), nrow(reference_data[[10]]))
   expect_equal(r, reference_data[[10]], ignore_attr = TRUE)
 })
 
-test_that("Results from the down knockdown signature are correct with respect to KD",{
+test_that("Results from the up knockdown signature are correct with respect to KD",{
   r <- get_concordants(input_data[[4]], "KD")
   expect_equal(nrow(r), nrow(reference_data[[11]]))
   expect_equal(r, reference_data[[11]], ignore_attr = TRUE)
 })
 
-test_that("Results from the down knockdown signature are correct with respect to CP",{
+test_that("Results from the up knockdown signature are correct with respect to CP",{
   r <- get_concordants(input_data[[4]], "CP")
   expect_equal(nrow(r), nrow(reference_data[[12]]))
   expect_equal(r, reference_data[[12]], ignore_attr = TRUE)
 })
-
-# Testing Chemical Perturbagen Signatures
 
 ## Testing the results for the up filtered signature
 test_that("Results from the up knockdown signature are correct with respect to OE",{
@@ -155,5 +153,65 @@ test_that("Results from the down knockdown signature are correct with respect to
   r <- get_concordants(input_data[[6]], "CP")
   expect_equal(nrow(r), nrow(reference_data[[18]]))
   expect_equal(r, reference_data[[18]], ignore_attr = TRUE)
+})
+
+# Testing Chemical Perturbagen Signatures
+
+## Testing the results for the any filtered signature
+test_that("Results from the up knockdown signature are correct with respect to OE",{
+  r <- get_concordants(input_data[[7]], "OE")
+  expect_equal(nrow(r), nrow(reference_data[[19]]))
+  expect_equal(r, reference_data[[19]], ignore_attr = TRUE)
+})
+
+test_that("Results from the up knockdown signature are correct with respect to KD",{
+  r <- get_concordants(input_data[[7]], "KD")
+  expect_equal(nrow(r), nrow(reference_data[[20]]))
+  expect_equal(r, reference_data[[20]], ignore_attr = TRUE)
+})
+
+test_that("Results from the up knockdown signature are correct with respect to CP",{
+  r <- get_concordants(input_data[[7]], "CP")
+  expect_equal(nrow(r), nrow(reference_data[[21]]))
+  expect_equal(r, reference_data[[21]], ignore_attr = TRUE)
+})
+
+
+## Testing the results for the up filtered signature
+test_that("Results from the up knockdown signature are correct with respect to OE",{
+  r <- get_concordants(input_data[[8]], "OE")
+  expect_equal(nrow(r), nrow(reference_data[[22]]))
+  expect_equal(r, reference_data[[22]], ignore_attr = TRUE)
+})
+
+test_that("Results from the up knockdown signature are correct with respect to KD",{
+  r <- get_concordants(input_data[[8]], "KD")
+  expect_equal(nrow(r), nrow(reference_data[[23]]))
+  expect_equal(r, reference_data[[23]], ignore_attr = TRUE)
+})
+
+test_that("Results from the up knockdown signature are correct with respect to CP",{
+  r <- get_concordants(input_data[[8]], "CP")
+  expect_equal(nrow(r), nrow(reference_data[[24]]))
+  expect_equal(r, reference_data[[24]], ignore_attr = TRUE)
+})
+
+## Testing the results for the down filtered signature
+test_that("Results from the down knockdown signature are correct with respect to OE",{
+  r <- get_concordants(input_data[[9]], "OE")
+  expect_equal(nrow(r), nrow(reference_data[[25]]))
+  expect_equal(r, reference_data[[25]], ignore_attr = TRUE)
+})
+
+test_that("Results from the down knockdown signature are correct with respect to KD",{
+  r <- get_concordants(input_data[[9]], "KD")
+  expect_equal(nrow(r), nrow(reference_data[[26]]))
+  expect_equal(r, reference_data[[26]], ignore_attr = TRUE)
+})
+
+test_that("Results from the down knockdown signature are correct with respect to CP",{
+  r <- get_concordants(input_data[[9]], "CP")
+  expect_equal(nrow(r), nrow(reference_data[[27]]))
+  expect_equal(r, reference_data[[27]], ignore_attr = TRUE)
 })
 
