67a5c2fba7b540fbc9c9ee28c1195181298f20bb
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-02-23 13:25:37 -0500

    consensus_concordants now returns the whole list

--- R/consensus_concordants.R
+++ R/consensus_concordants.R
@@ -32,7 +32,6 @@ target_rename <- function(input_names) {
 #' @param paired Logical indicating whether you split the dataframes by up and down regulated in prior analysis
 #' @param cutoff A similarity cutoff value. Defaults to 0.321
 #' @param cell_line A character vector of Cell Lines you are interested in.
-#' @param discordant Logical indicating whether you want discordant signatures
 #'
 #' @return A tibble with the filtered and deduplicated results
 #' @export
@@ -44,7 +43,7 @@ target_rename <- function(input_names) {
 #' @examples
 #' TRUE
 consensus_concordants <- function(..., paired = FALSE, cutoff = 0.321,
-                                  cell_line = NULL, discordant = FALSE) {
+                                  cell_line = NULL) {
     if (paired & length(list(...)) != 2) {
         stop("Paired analysis requires two data frames")
     } else if (!paired & length(list(...)) != 1) {
@@ -58,37 +57,20 @@ consensus_concordants <- function(..., paired = FALSE, cutoff = 0.321,
             dplyr::filter(.data$cellline %in% cell_line)
     }
 
-    if (discordant) {
-        filtered <- concordants %>%
-            dplyr::filter(.data$similarity <= -cutoff) %>%
-            dplyr::group_by(
-                dplyr::across(dplyr::any_of(c("treatment", "compound")))
-            ) %>%
-            dplyr::filter(abs(.data$similarity) == max(abs(.data$similarity))) %>%
-            dplyr::select(
-                .data$signatureid, dplyr::any_of(c("treatment", "compound")),
-                .data$cellline, .data$time, dplyr::any_of(c("concentration")),
-                .data$similarity
-            ) %>%
-            dplyr::arrange(dplyr::desc(abs(.data$similarity))) %>%
-            dplyr::rename_with(target_rename) %>%
-            dplyr::ungroup()
-    } else {
-        filtered <- concordants %>%
-            dplyr::filter(.data$similarity >= cutoff) %>%
-            dplyr::group_by(
-                dplyr::across(dplyr::any_of(c("treatment", "compound")))
-            ) %>%
-            dplyr::filter(abs(.data$similarity) == max(abs(.data$similarity))) %>%
-            dplyr::select(
-                .data$signatureid, dplyr::any_of(c("treatment", "compound")),
-                .data$cellline, .data$time, dplyr::any_of(c("concentration")),
-                .data$similarity
-            ) %>%
-            dplyr::arrange(dplyr::desc(abs(.data$similarity))) %>%
-            dplyr::rename_with(target_rename) %>%
-            dplyr::ungroup()
-    }
+    filtered <- concordants %>%
+        dplyr::filter(abs(.data$similarity) >= cutoff) %>%
+        dplyr::group_by(
+            dplyr::across(dplyr::any_of(c("treatment", "compound")))
+        ) %>%
+        dplyr::filter(abs(.data$similarity) == max(abs(.data$similarity))) %>%
+        dplyr::select(
+            .data$signatureid, dplyr::any_of(c("treatment", "compound")),
+            .data$cellline, .data$time, dplyr::any_of(c("concentration")),
+            .data$similarity
+        ) %>%
+        dplyr::arrange(dplyr::desc(abs(.data$similarity))) %>%
+        dplyr::rename_with(target_rename) %>%
+        dplyr::ungroup()
 
     filtered
 }
