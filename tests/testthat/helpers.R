449d2eedab795862969a43779688826dec079fb1
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-09-11 22:31:14 -0400

    test(get_concordants): add tests and fixtures for get_concordants

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -27,6 +27,24 @@ example_signature <- function() {
     if (file.exists(file.path(test_path(), "fixtures", "example_signature.csv"))) {
         readr::read_csv(file.path(test_path(), "fixtures", "example_signature.csv"), col_types = "cccnn")
     } else {
-        get_signature("LINCSKD_28")
+        get_signature("LINCSKD_28") |>
+            readr::write_csv(file.path(test_path(), "fixtures", "example_signature.csv"))
+    }
+}
+
+## Generate concordants for a signature
+
+concordants_cp <- function() {
+    if (file.exists(file.path(test_path(), "fixtures", "concordants_cp.csv"))) {
+        readr::read_csv(file.path(test_path(), "fixtures", "concordants_cp.csv"), col_types = "cccccnnc")
+    } else {
+        get_concordants(
+            {
+                example_signature() |> filter_signature(threshold = 1.0)
+            },
+            "CP",
+            "any"
+        ) |>
+            readr::write_csv(file.path(test_path(), "fixtures", "concordants_cp.csv"))
     }
 }
---
+++ tests/testthat/test-get_concordants.R
@@ -0,0 +1,27 @@
+# Test the `get_concordants` function
+
+# Test Invalid inputs
+
+test_that("Input signature must be a data frame or data frame like object", {
+    expect_error(get_concordants("LINCSKD_28"))
+})
+
+test_that("library must be one of 'OE', 'KD' or 'CP'", {
+    expect_error(get_concordants(example_signature(), "INVALID"))
+})
+
+# Test invalid signature
+
+
+# Test valid signature
+
+test_that("get_concordants correct value", {
+    input_signature <- example_signature() |>
+        filter_signature(threshold = 1.0)
+    concordants_list <- get_concordants(input_signature, "CP", "any")
+    expect_s3_class(concordants_list, "tbl_df")
+    expect_equal(concordants_list, concordants_cp(), tolerance = 1e-12)
+    expect_identical(ncol(concordants_list), 8L)
+    expect_identical(nrow(concordants_list), 14337L)
+    expect_identical(unique(concordants_list[["sig_direction"]]), "any")
+})
