75d3122b7cf71f58363e5574bb7167a5a7369201
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-04-22 21:02:37 +0500

    docs: add description to get_signature
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- R/get_signature.R
+++ R/get_signature.R
@@ -2,6 +2,12 @@
 #'
 #' `r lifecycle::badge("experimental")`
 #'
+#' This function acts as the entrypoint to the iLINCS database.
+#' This takes in an ID and returns the signature after making a call to the iLINCS
+#' database. The default mode for `drugfindR` is to use L1000 signatures. However,
+#' if you are trying to retrieve a different transcriptomic signature, that is also supported
+#' by setting the `l1000` parameter to `FALSE`.
+#'
 #' @param sig_id character. The ilincs signature_id
 #' @param l1000 boolean. If you have a known l1000 signature
 #'
@@ -16,7 +22,14 @@
 #' @importFrom purrr map_dfr
 #'
 #' @examples
-#' TRUE
+#' \dontrun{
+#'
+#' # Get the L1000 signature for LINCSKD_28
+#' kd_signature <- getSignature("LINCSKD_28")
+#'
+#' # Get the non-L1000 signature for EBI_1001
+#' ebi_signature <- getSignature("EBI_1001")
+#' }
 getSignature <- function(sigId, l1000 = TRUE) {
     url <- "http://www.ilincs.org/api/ilincsR/downloadSignature"
 
