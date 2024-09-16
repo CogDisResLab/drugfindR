b132ddd963b02ce6f301001d0ac3628d4e6daea6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-09-16 12:38:46 -0400

    feat: update the getSignature() method to use httr2
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- R/getSignature.R
+++ R/getSignature.R
@@ -17,8 +17,8 @@
 #' @export
 #'
 #' @importFrom httr POST content status_code
+#' @importFrom httr2 request req_method req_url_query req_user_agent req_url_path_append req_perform resp_status resp_body_json resp_body_string
 #' @importFrom tibble tibble as_tibble
-#' @importFrom magrittr %>%
 #' @importFrom rlang .data
 #' @importFrom dplyr select
 #' @importFrom purrr map_dfr
@@ -28,32 +28,31 @@
 #' # Get the L1000 signature for LINCSKD_28
 #' kdSignature <- getSignature("LINCSKD_28")
 getSignature <- function(sigId, l1000 = TRUE) {
-    url <- "http://www.ilincs.org/api/ilincsR/downloadSignature"
-
     if (l1000) {
         numGenes <- 978L
     } else {
         numGenes <- Inf
     }
 
-    query <- list(sigID = sigId, noOfTopGenes = numGenes)
+    request <- httr2::request(.ilincsBaseUrl()) |>
+        httr2::req_url_path_append("downloadSignature") |>
+        httr2::req_url_query(sigID = sigId, noOfTopGenes = numGenes) |>
+        httr2::req_method("POST") |>
+        httr2::req_user_agent("drugfindR/v0.99.980; https://github.com/CogDisResLab/drugfindR") |>
+        httr2::req_perform()
 
-    request <- httr::POST(url, query = query)
 
-    if (httr::status_code(request) == 200L) {
-        signature <- httr::content(request) %>%
-            purrr::map("signature") %>%
-            purrr::flatten_dfr() %>%
-            dplyr::select(-"PROBE") %>%
+    if (httr2::resp_status(request) == 200L) {
+        signature <- httr2::resp_body_json(request) |>
+            purrr::map("signature") |>
+            purrr::flatten_dfr() |>
+            dplyr::select(-"PROBE") |>
             dplyr::mutate(
                 ID_geneid = as.character(.data[["ID_geneid"]]),
                 Value_LogDiffExp = round(.data[["Value_LogDiffExp"]], 12L),
                 Significance_pvalue = round(.data[["Significance_pvalue"]], 12L)
             )
-        signature %>%
-            S4Vectors::DataFrame() %>%
-            as_tibble()
     } else {
-        stop(httr::status_code(request), " ", httr::content(request))
+        stop(httr2::resp_status(request), " ", httr2::resp_body_string(request))
     }
 }
